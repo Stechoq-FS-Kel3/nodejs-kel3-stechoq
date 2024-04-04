f308d262389fbd43bf1bb1fc0b434abc91c67a10
dhenmasadhi <dhenmasa@gmail.com>
2024-04-04 13:49:54 +0700

    menambhakan index.js

---
+++ src/db.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  port: 9706,
+  user: "userpk",
+  password: "",
+  database: "pengelolaan_keuangan",
+});
+
+exports.query = async (sql, values = []) => {
+  try {
+    const koneksi = await pool.getConnection();
+    const barisData = await koneksi.query(sql, values);
+    koneksi.release();
+    return barisData;
+  } catch (error) {
+    throw error;
+  }
+};
